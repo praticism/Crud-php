85c8051701e989da4ca064812a53ada69c3f24dd
praticism <prashanttamang64bs@gmail.com>
2024-06-09 16:30:25 +0545

    first commit

---
+++ dbConnection.php
@@ -0,0 +1,8 @@
+<?php
+$databaseHost = 'localhost';
+$databaseName = 'crud_db';
+$databaseUsername = 'root';
+$databasePassword = 'root';
+
+// Open a new connection to the MySQL server
+$mysqli = mysqli_connect($databaseHost, $databaseUsername, $databasePassword, $databaseName); 
